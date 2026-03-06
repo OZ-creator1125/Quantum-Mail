732ffa2252a9e45feadeba28c44de7197800b23f
OZ-creator1125 <coz_dita@hotmail.com>
2026-03-06 15:27:29 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import TemporaryEmail from "./pages/TemporaryEmail";
+import TempMail from "./pages/TempMail";
+import FakeEmail from "./pages/FakeEmail";
+import TenMinuteEmail from "./pages/TenMinuteEmail";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* Página principal */}
         <Route path="/" element={<Home />} />
 
-        {/* Página SEO */}
         <Route path="/temporary-email" element={<TemporaryEmail />} />
+        <Route path="/temp-mail" element={<TempMail />} />
+        <Route path="/fake-email" element={<FakeEmail />} />
+        <Route path="/10-minute-email" element={<TenMinuteEmail />} />
 
       </Routes>
     </BrowserRouter>
