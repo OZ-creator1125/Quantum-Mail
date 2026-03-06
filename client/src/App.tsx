f1592325281f06037694e5a4c99a4698fa00845a
OZ-creator1125 <coz_dita@hotmail.com>
2026-03-06 11:39:49 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import Contact from "./pages/Contact";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const path = window.location.pathname;
+
+  if (path === "/privacy") {
+    return <Privacy />;
+  }
+
+  if (path === "/terms") {
+    return <Terms />;
+  }
+
+  if (path === "/contact") {
+    return <Contact />;
+  }
+
+  return <Home />;
 }
