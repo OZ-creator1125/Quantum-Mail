3d4b9608d56cab225b01d202383a923e02af2b85
OZ-creator1125 <coz_dita@hotmail.com>
2026-03-09 16:03:28 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,15 +10,29 @@ export default function App() {
     <BrowserRouter>
       <Routes>
 
+        {/* HOME */}
         <Route path="/" element={<Home />} />
 
-        {/* SEO PAGES */}
-        <Route path="/temporary-email" element={<Home />} />
+        {/* SEO ENGLISH */}
         <Route path="/temp-mail" element={<Home />} />
+        <Route path="/temporary-email" element={<Home />} />
         <Route path="/disposable-email" element={<Home />} />
         <Route path="/10-minute-email" element={<Home />} />
         <Route path="/free-temp-mail" element={<Home />} />
         <Route path="/anonymous-email" element={<Home />} />
+        <Route path="/burner-email" element={<Home />} />
+        <Route path="/random-email-generator" element={<Home />} />
+        <Route path="/temp-email-generator" element={<Home />} />
+        <Route path="/secure-temp-mail" element={<Home />} />
+
+        {/* SEO ESPAÑOL */}
+        <Route path="/correo-temporal" element={<Home />} />
+        <Route path="/email-temporal" element={<Home />} />
+        <Route path="/correo-desechable" element={<Home />} />
+        <Route path="/correo-anonimo" element={<Home />} />
+        <Route path="/generador-email" element={<Home />} />
+        <Route path="/email-temporal-gratis" element={<Home />} />
+        <Route path="/correo-10-minutos" element={<Home />} />
 
         {/* LEGAL */}
         <Route path="/privacy" element={<Privacy />} />
