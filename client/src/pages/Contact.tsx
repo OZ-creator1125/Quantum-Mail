b1bc3bbf3fc5abd5bd985b819b5a244f34d824b2
OZ-creator1125 <coz_dita@hotmail.com>
2026-03-09 14:44:11 -0600

    Update Contact.tsx

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,23 +1,20 @@
 export default function Contact() {
   return (
     <div className="qm-shell min-h-screen">
-      <div className="qm-wrap">
-        <div className="qm-panel p-6">
-          <h1 className="qm-about__title">Contact</h1>
+      <div className="qm-wrap" style={{maxWidth:900,margin:"0 auto"}}>
+        <h1 style={{marginBottom:20}}>Contact</h1>
 
-          <p className="qm-about__text">
-            If you need assistance or want to report abuse of the service, you
-            can contact us.
-          </p>
+        <p>
+          For support, feedback or inquiries please contact us at:
+        </p>
 
-          <p className="qm-about__text">
-            Email: support@qmailtemp.com
-          </p>
+        <p style={{marginTop:20,fontWeight:"bold"}}>
+          support@qmailtemp.com
+        </p>
 
-          <p className="qm-about__text">
-            We will respond as soon as possible.
-          </p>
-        </div>
+        <p style={{marginTop:20}}>
+          We will respond as soon as possible.
+        </p>
       </div>
     </div>
   );
