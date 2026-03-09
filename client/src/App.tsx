5dcf39dd957104339052f7e30cfa8637b986b9c7
OZ-creator1125 <coz_dita@hotmail.com>
2026-03-09 15:20:15 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
@@ -8,10 +9,22 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Home />} />
+
+        {/* SEO PAGES */}
+        <Route path="/temporary-email" element={<Home />} />
+        <Route path="/temp-mail" element={<Home />} />
+        <Route path="/disposable-email" element={<Home />} />
+        <Route path="/10-minute-email" element={<Home />} />
+        <Route path="/free-temp-mail" element={<Home />} />
+        <Route path="/anonymous-email" element={<Home />} />
+
+        {/* LEGAL */}
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="/contact" element={<Contact />} />
+
       </Routes>
     </BrowserRouter>
   );
