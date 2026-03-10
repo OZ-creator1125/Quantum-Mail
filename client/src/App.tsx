92ae29f281e9cabdcb3fe3cb3c81bd7267ed55c2
OZ-creator1125 <coz_dita@hotmail.com>
2026-03-10 10:32:47 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,26 +4,22 @@ import Home from "./pages/Home";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import Contact from "./pages/Contact";
-
-import TempMail from "./pages/seo/TempMail";
-import TemporaryEmail from "./pages/seo/TemporaryEmail";
-import TenMinuteEmail from "./pages/seo/TenMinuteEmail";
-import FakeEmail from "./pages/seo/FakeEmail";
+import TempMail from "./pages/TempMail";
+import TemporaryEmail from "./pages/TemporaryEmail";
+import TenMinuteEmail from "./pages/TenMinuteEmail";
+import FakeEmail from "./pages/FakeEmail";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* APP */}
         <Route path="/" element={<Home />} />
 
-        {/* SEO PAGES */}
         <Route path="/temp-mail" element={<TempMail />} />
         <Route path="/temporary-email" element={<TemporaryEmail />} />
         <Route path="/10-minute-email" element={<TenMinuteEmail />} />
         <Route path="/fake-email-generator" element={<FakeEmail />} />
 
-        {/* LEGAL */}
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="/contact" element={<Contact />} />
