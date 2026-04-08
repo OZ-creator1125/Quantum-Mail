9fb58c4c305d344d62da02449fdf739abe8e90a0
Omar Zurita <macoz@MacBook-Air-de-Omar.local>
2026-04-08 13:14:22 -0600

    final fix api

--- server/api/inbox.ts
+++ server/api/inbox.ts
@@ -2,17 +2,21 @@ let emails: any[] = [];
 
 export default function handler(req: any, res: any) {
 
+  // POST → guardar email
   if (req.method === "POST") {
+    const data = req.body;
+
     emails.unshift({
-      ...req.body,
+      ...data,
       id: Date.now()
     });
 
-    console.log("📩 EMAIL:", req.body);
+    console.log("📩 EMAIL:", data);
 
     return res.status(200).json({ ok: true });
   }
 
+  // GET → devolver emails
   if (req.method === "GET") {
     return res.status(200).json(emails);
   }
